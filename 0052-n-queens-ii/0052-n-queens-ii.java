097ebd346e874674c403dd970d20baf46ed8aa81
praneethsaithota <praneethsaithota@gmail.com>
2025-07-31 02:58:32 +0530

    Time: 2 ms (64.44%), Space: 40.6 MB (41.42%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,63 @@
+
+class Solution {
+    int ans=0;
+    public void function(char[][] chessboard,int col){
+        if(col == chessboard.length){
+            ans++;
+            return;
+        }
+        for (int row=0;row<chessboard.length;row++){
+            if(safeornot(row,col,chessboard)){
+                chessboard[row][col]='Q';
+                function(chessboard,col+1);
+                chessboard[row][col]='.';
+            }
+        }
+    }
+    public boolean safeornot(int row,int col,char[][] chessboard){
+        for(int j=0;j<chessboard.length;j++){
+            if(chessboard[row][j]=='Q'){
+                return false;
+            }
+        }
+        for(int j=0;j<chessboard.length;j++){
+            if(chessboard[j][col]=='Q'){
+                return false;
+            }
+        }
+        int r=row;
+        for(int c=col;c>=0 && r>=0;c--,r--){
+           if (chessboard[r][c]=='Q'){
+                return false;
+           }
+        }
+        r=row;
+        for(int c=col;c>=0 && r<chessboard.length;c--,r++){
+           if (chessboard[r][c]=='Q'){
+                return false;
+           }
+        }
+        r=row;
+        for(int c=col;c<chessboard.length && r>=0;c++,r--){
+           if (chessboard[r][c]=='Q'){
+                return false;
+           }
+        }
+        r=row;
+        for(int c=col;c<chessboard.length && r<chessboard.length;c++ , r++){
+           if (chessboard[r][c]=='Q'){
+                return false;
+           }
+        }
+        return true;
+
+        
+    }
+    
+    public int totalNQueens(int n) {
+        
+        char[][] chessboard=new char[n][n];
+        function(chessboard,0);
+        return ans;
+    }
+}
