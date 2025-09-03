f6928596b788658fc20823a09545b04e3a567f31
praneethsaithota <praneethsaithota@gmail.com>
2025-09-04 00:31:06 +0530

    Time: 3 ms (29.33%), Space: 11.7 MB (71.04%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>sol;
+        for(int i=1;i<=n;i++){
+            if(i%3!=0&&i%5!=0){
+                sol.push_back(to_string(i));
+            }
+            else if(i%3==0&&i%5!=0)
+                sol.push_back("Fizz");
+            else if(i%3!=0&&i%5==0)
+                sol.push_back("Buzz");
+            else 
+                sol.push_back("FizzBuzz");
+        }
+        return sol;
+    }
+};
