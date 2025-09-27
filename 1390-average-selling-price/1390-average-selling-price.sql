051f73ec74374070f544557f17f6f4eecf879c16
praneethsaithota <praneethsaithota@gmail.com>
2025-09-28 00:20:08 +0530

    Time: 840 ms (34.21%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,18 +1,8 @@
-WITH products AS (
-  SELECT DISTINCT product_id FROM Prices
-),
-sales AS (
-  SELECT u.product_id,
-         SUM(u.units * p.price) AS total_revenue,
-         SUM(u.units) AS total_units
-  FROM UnitsSold u
-  JOIN Prices p
-    ON u.product_id = p.product_id
-   AND u.purchase_date BETWEEN p.start_date AND p.end_date
-  GROUP BY u.product_id
-)
-SELECT pr.product_id,
-       COALESCE(ROUND(s.total_revenue / s.total_units, 2), 0) AS average_price
-FROM products pr
-LEFT JOIN sales s ON pr.product_id = s.product_id
-ORDER BY pr.product_id;
+SELECT p.product_id,
+       COALESCE(ROUND(SUM(u.units * p.price) / SUM(u.units), 2), 0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+  ON p.product_id = u.product_id
+ AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
+ORDER BY p.product_id;
