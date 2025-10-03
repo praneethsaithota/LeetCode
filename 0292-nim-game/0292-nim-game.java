16b1fa6d482929d1aec1e3534343ee9a6b7c9852
praneethsaithota <praneethsaithota@gmail.com>
2025-10-03 23:34:42 +0530

    Time: 0 ms (100%), Space: 40.6 MB (38.68%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n==1 || n==2 || n==3){
+            return true;
+        }
+        if(n%4==0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
