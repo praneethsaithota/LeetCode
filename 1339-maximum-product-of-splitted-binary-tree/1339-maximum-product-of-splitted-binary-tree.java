594bb99b47f96855b791dd97fc65c8d727cde096
praneethsaithota <praneethsaithota@gmail.com>
2026-01-08 01:56:14 +0530

    Time: 7 ms (47.67%), Space: 59.8 MB (72.87%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -1,31 +1,22 @@
-class Solution {
-    long mod=1000000007;
+class Solution{
+    long max=0;
+    int mod=1000000007;
 
     public int find_total(TreeNode root){
         if(root==null) return 0;
         return root.val+find_total(root.left)+find_total(root.right);
     }
 
+    public int maximum(TreeNode root,int total){
+        if(root==null) return 0;
+        int temp=root.val+maximum(root.left,total)+maximum(root.right,total);
+        max=Math.max(max,(long)temp*(total-temp));
+        return temp;
+    }
+
     public int maxProduct(TreeNode root){
         int total=find_total(root);
-        manipulate(root);
-        return (int)(traverse(root,total)%mod);
-    }
-
-    public int manipulate(TreeNode root){
-        if(root==null) return 0;
-        if(root.left==null && root.right==null) return root.val;
-        root.val=root.val + manipulate(root.left)+manipulate(root.right);
-        return root.val;
-    }
-
-    public long traverse(TreeNode root,int total){
-        if(root==null) return 0;
-
-        long temp=(long)root.val*(total-root.val);
-        long ans1=Math.max(temp,traverse(root.left,total));
-        long ans2=Math.max(temp,traverse(root.right,total));
-
-        return Math.max(ans1,ans2);
+        maximum(root,total);
+        return (int)(max%mod);
     }
 }
